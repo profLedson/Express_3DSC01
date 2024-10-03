22f42429d0412939edf671b941b3ac0b58620751
LEDSON J VANINI <ledson.vanini@escola.pr.gov.br>
2024-10-03 11:41:18 +0000

    Criadas as rotas home e sobre

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3301;
+const PORT = 3303;
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/src/views/");
-  //console.log(__dirname + "/src/views");
+  res.sendFile(__dirname + "/src/views/index.html");
+  //console.log(__dirname + "/src/views/index.html");
 });
+
 app.listen(PORT, () => {
   console.log("running...");
 });
