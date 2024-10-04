2fda359280c493922a2ae35792dd33940c960fdd
LEDSON J VANINI <ledson.vanini@escola.pr.gov.br>
2024-10-04 15:04:18 +0000

    Criadas as rotas home e sobre. Iniciado o menu

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const PORT = 3303;
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/src/views/index.html");
-  //console.log(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+// ROTA UTILIZANDO MIDDLEWARE(404)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
