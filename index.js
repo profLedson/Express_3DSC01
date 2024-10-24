7671f2e96f9a6254aa14114619854c428bf40900
LEDSON J VANINI <ledson.vanini@escola.pr.gov.br>
2024-10-24 11:54:27 +0000

    Middleware da pasta public + css menu

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 const PORT = 3303;
+// CONFIGURANDO ROTA DE ESTÁTICOS
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/src/views/index.html");
